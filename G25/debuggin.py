2595a7db465a1f35080b88157bc0f6750b4ee666
JParrales <josedparrales@gmail.com>
2021-06-01 15:35:32 -0500

    debugg

---
+++ G25/debuggin.py
@@ -0,0 +1,15 @@
+def divisores(numero):
+    divisor = []
+
+    for i in range(1, numero + 1):
+        if numero % i == 1:
+            divisor.append(i)
+    
+    return divisor
+
+
+if __name__ == '__main__':
+
+    num = int(input("Ingrese un numero: "))
+    print(divisores(num))
+    print("---FIN PROGRAMA")
