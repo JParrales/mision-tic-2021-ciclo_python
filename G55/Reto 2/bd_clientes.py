5ee2907441e28f4b11cf3bd4f60e13d9bfd9519a
JParrales <josedparrales@gmail.com>
2021-06-08 10:34:15 -0500

    RETO 2 - 55

---
+++ G55/Reto 2/bd_clientes.py
@@ -0,0 +1,47 @@
+def bd_autos():
+
+    autos = {
+
+        'STARK 01': {
+            'marca': 'Ford',
+            'nombre': 'flathead roadster',
+            'año': 1932,
+            'motor': 'V8',
+            'caballos': 85,
+            'disponible': False
+        },
+        'STARK 02': {
+            'marca': 'Ford',
+            'nombre': 'shelby',
+            'año': 1967,
+            'motor': 'V8',
+            'caballos': 427,
+            'disponible': False
+        },
+        'STARK 04': {
+            'marca': 'Saleen',
+            'nombre': 'S7',
+            'año': 2009,
+            'motor': 'V8',
+            'caballos': 760,
+            'disponible': True
+        },
+        'STARK 12': {
+            'marca': 'Tesla',
+            'nombre': 'roadster',
+            'año': 2009,
+            'motor': 'V8',
+            'caballos': 292,
+            'disponible': False
+        },
+        'STARK 08': {
+            'marca': 'Audi',
+            'nombre': 'R8',
+            'año': 2008,
+            'motor': 'V10',
+            'caballos': 525,
+            'disponible': True
+        },
+    }
+
+    return autos
