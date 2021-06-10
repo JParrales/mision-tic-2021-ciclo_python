10eda4c2948f42b2c65c71b405af87d47f1a2507
JParrales <josedparrales@gmail.com>
2021-06-10 15:59:16 -0500

    xx

--- G85/Reto 2/practica.py
+++ G85/Reto 2/practica.py
@@ -9,6 +9,48 @@ la función debe actualizar el objeto en la base de datos.
 • La llave con la cual se identificará el objeto será de tipo string, 
 si el tipo de la llave es diferente, debe solicitar un identificador válido para el objeto.
 """
+# print(clientes[cliente]) 
+# print(clientes[cliente["nombre"]]) 
+def bd_objetos():
 
+    objetos = {
 
+        'Mark I': {
+            'color': 'Gris',
+            'material': 'metal',
+            'peso kg': 360,
+            'sistema': 'MS-DOS',
+            'disponible': False
+        },
+        'Mark II': {
+            'color': 'Gris',
+            'material': 'metal',
+            'peso kg': 280,
+            'sistema': 'JARVIS',
+            'disponible': True
+        },
+        'Mark III': {
+            'color': 'Rojo Y Oro',
+            'material': 'oro y titanio',
+            'peso kg': 280,
+            'sistema': 'JARVIS',
+            'disponible': True
+        },
+        'Mark VII': {
+            'color': 'Rojo Y Oro',
+            'material': 'oro y titanio',
+            'peso kg': 200,
+            'sistema': 'JARVIS',
+            'disponible': False
+        },
+        'Veronica I': {
+            'color': 'Rojo Y Oro',
+            'material': 'acero, hierro y niquel',
+            'peso kg': 975,
+            'sistema': 'JARVIS',
+            'disponible': True
+        },
+    }
+
+    return objetos
 
