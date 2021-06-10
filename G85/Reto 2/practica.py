06862850f8cc5234086f1843c276a4cd5e038d84
JParrales <josedparrales@gmail.com>
2021-06-10 15:57:34 -0500

    enunciado

---
+++ G85/Reto 2/practica.py
@@ -0,0 +1,14 @@
+""" 
+
+• Crear un objeto en la base de datos con sus especificaciones,
+esto se realizará a través de un diccionario.
+• Al intentar crear un objeto que ya se encuentre en la base de datos, 
+solo verificará su existencia.
+• Si un objeto se encuentra en la base de datos y sus especificaciones no coinciden 
+la función debe actualizar el objeto en la base de datos.
+• La llave con la cual se identificará el objeto será de tipo string, 
+si el tipo de la llave es diferente, debe solicitar un identificador válido para el objeto.
+"""
+
+
+
